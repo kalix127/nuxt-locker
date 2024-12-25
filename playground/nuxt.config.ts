2d3b4da7cd40e41ac44c0ae8db7b55efb4debdb8
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-12-25 20:37:06 +0100

    style: lint

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
-  myModule: {},
+  modules: ["../src/module"],
   devtools: { enabled: true },
-})
+  compatibilityDate: "2024-12-25",
+  myModule: {},
+});
